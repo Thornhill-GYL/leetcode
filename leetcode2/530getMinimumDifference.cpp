b39d7863b330b22ae305ac626a39cd838e396588
Thornhill-GYL <Thornhill-GYL@outlook.com>
2020-10-12 23:34:41 +0800

    二叉搜索树的相邻节点的最小绝对值的递归方法完成；非递归和建立二叉搜索树的方法需要补充

--- leetcode2/530getMinimumDifference.cpp
+++ leetcode2/530getMinimumDifference.cpp
@@ -37,4 +37,35 @@ struct TreeNode{
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-vector<int> treenum {3,9,20,NULL,NULL,15,7};
+/*
+递归的方法：
+受到启发，中序遍历刚好可以从左边到中间再到右边
+只需要计算当前的和之前的节点的插值
+*/
+int min_num = INT_MAX;
+TreeNode* pre;//这个地方的不能初始化，不然程序默认它是零，得到错误的结果
+void inorder(TreeNode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	//中序遍历，将左子树存入
+	inorder(root->left);
+	if (pre != NULL)
+	{
+		min_num = min(min_num, abs(root->val - pre->val));
+	}
+	pre = root;//记录当前节点
+	//右节点存入
+	inorder(root->right);
+}
+/*
+非递归方法需要补充
+以及建立二叉搜索树的code也需要补充
+*/
+//官方函数
+int getMinimumDifference(TreeNode* root) {
+	inorder(root);
+	return min_num;
+}
