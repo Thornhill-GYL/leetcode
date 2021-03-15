d2522a5907308200e67a36d893b5c770e14d1466
DUTIR_GYL_person <Thornhill-gyl@outlook.com>
2021-03-15 14:54:51 +0800

    T2:二分法、线性（二叉搜索树）

--- offerleetcode/offer2.cpp
+++ offerleetcode/offer2.cpp
@@ -1,7 +1,7 @@
 /*
 @time:2021-03-15
 @author:gyl
-@serise:offer1
+@serise:offer2
 @problem:在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
  
@@ -21,7 +21,7 @@
 
 给定 target = 20，返回 false。
 
-
+Push 更新
 */
 #include <iostream>
 #include <vector>
